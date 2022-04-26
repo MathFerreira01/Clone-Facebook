ef22bf2922ac34c135788f5563ccf72e572b0b20
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-04-26 20:58:44 -0300

    adicionado o banco de dados do firebase para coletar as informações dos posts

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,27 +1,81 @@
 import Image from "next/image";
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
+
+import { db, storage } from "../firebase";
+import firebase from "firebase";
 
 import User from "../assets/facebook-user.jpg";
 
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
-import { db } from "../firebase";
+
 
 function InputBox() {
   const inputRef = useRef(null);
+  const filepickerRef = useRef(null);
+  const [imageToPost, setImageToPost] = useState(null);
 
   const sendPost = (e) => {
     e.preventDefault();
 
-    if(!inputRef.current.value) return;
+    if (!inputRef.current.value) return;
 
-    db.collection('posts').add({
-      message: inputRef
-    })
+    db.collection("posts")
+      .add({
+        message: inputRef.current.value,
+        name: "Matheus F",
+        email: "matheusf123@gmail.com",
+        image: { User },
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, "data_url");
+
+          removeImage();
+
+          uploadTask.on(
+            "state_change",
+            null,
+            (error) => console.error(error),
+            () => {
+              // when the upload completes
+              storage
+                .ref("posts")
+                .child(doc.id)
+                .getDownloadURL()
+                .then((url) => {
+                  db.collection("posts").doc(doc.id).set(
+                    {
+                      postImage: url,
+                    },                   
+                      {merge: true}                   
+                  )
+                })
+            }
+          );
+        }
+      });
+
+    inputRef.current.value = "";
   };
 
-  
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
 
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
+  };
 
   return (
     <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
@@ -45,15 +99,34 @@ function InputBox() {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
+          >
+            <img className="h-10 object-contain" src={imageToPost} alt="" />
+            <p className="text-xs text-red-500 text-center">Remove</p>
+          </div>
+        )}
       </div>
       <div className="flex justify-evenly p-3 border-t">
         <div className="inputIcon">
           <CameraIcon className="h-7 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
-        <div className="inputIcon">
+        <div
+          onClick={() => filepickerRef.current.click()}
+          className="inputIcon"
+        >
           <VideoCameraIcon className="h-7 text-green-400" />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          <input
+            ref={filepickerRef}
+            type="file"
+            onChange={addImageToPost}
+            hidden
+          />
         </div>
         <div className="inputIcon">
           <EmojiHappyIcon className="h-7 text-yellow-300" />
